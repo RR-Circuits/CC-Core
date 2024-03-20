abf7985dc1a87d063e156aa56c05fbd8bb4b9b59
FunnPunn <rrobuxian@gmail.com>
2024-03-20 09:51:23 +0100

    pushing this for now

--- Website/main.js
+++ Website/main.js
@@ -1,20 +1,42 @@
-var selectedChip, offset, transform;
+var selectedChip, offset, transform, SVGGroup;
 var svg = null
-
 var currentSize = 1
-
-function zoomIn(evt) {
-    if (!currentSize > 2) {
-        currentSize += 0.25
-        updateSize()
-    }
+var posFromCenter = {
+    x: 0,
+    y: 0
 }
 
-function zoomOut(evt) {
-    if (!currentSize < 0.50) {
-        currentSize -= 0.25
-        updateSize()
+const r = document.querySelector(":root")
+
+const ControlMode = {
+    Default: Symbol("default"),
+    Touchpad: Symbol("touchpad"),
+    Mobile: Symbol("mobile")
+}
+
+var currentControlMode = ControlMode.Default
+
+function changeTouchpad() {
+    currentControlMode = ControlMode.Touchpad
+    console.log(currentControlMode)
+}
+
+function changeMouse() {
+    currentControlMode = ControlMode.Default
+    console.log(currentControlMode)
+}
+
+function DetectDevice() {
+    let isMobile = window.matchMedia || window.msMatchMedia;
+    if (isMobile) {
+      let match_mobile = isMobile("(pointer:coarse)");
+      return match_mobile.matches;
     }
+    return false;
+}
+
+if (DetectDevice()) {
+    currentControlMode = ControlMode.Mobile
 }
 
 function updateSize() {
@@ -36,6 +58,35 @@ function changeMode(newMode) {
     }
 }
 
+function changeBG(deltaX, deltaY) {
+    /*let offX, offY
+
+    let oldX = Number(rs.getPropertyValue("--bg-x").replace(/^\D+/g, ''))
+    let oldY = Number(rs.getPropertyValue("--bg-y").replace(/^\D+/g, ''))
+
+    console.log("x" + oldX + "y" + oldY)
+
+    oldX = Math.abs(oldX)
+    oldY = Math.abs(oldY)
+
+    offX = deltaX
+    offY = deltaY
+
+    if (deltaX == null || deltaY == null) {
+        offX = offset.x
+        offY = offset.y
+    }
+
+    r.style.setProperty("--bg-x", oldX + offX + "px")
+    r.style.setProperty("--bg-y", oldY + offY + "px")*/
+
+    let r = document.querySelector(":root")
+    let rs = getComputedStyle(r)
+
+    r.style.setProperty("--bg-x", deltaX + "px")
+    r.style.setProperty("--bg-y", deltaY + "px")
+}
+
 function translateOffset(el) {
     var transforms = el.transform.baseVal;
     // Ensure the first transform is a translate transform
@@ -51,6 +102,7 @@ function translateOffset(el) {
 
 function dragStart(evt) {
     //console.log(evt.target)
+    if (currentControlMode == ControlMode.Mobile) return;
     evt.preventDefault()
     switch (evt.button) {
         case 0:
@@ -78,11 +130,31 @@ function dragStart(evt) {
     }
 }
 
+function scrolling(evt) {
+    if (currentControlMode != ControlMode.Touchpad) return;
+    evt.preventDefault()
+
+    transform = translateOffset(SVGGroup).getItem(0);
+
+    transform.setTranslate(transform.matrix.e - evt.deltaX, transform.matrix.f - evt.deltaY)
+    posFromCenter.x -= evt.deltaX
+    posFromCenter.y -= evt.deltaY
+
+    changeBG(posFromCenter.x, posFromCenter.y)
+}
+
 function dragMove(evt) {
     evt.preventDefault()
     var mouse = getMousePosition(evt)
     if (selectedChip) {
+        SVGGroup.append(selectedChip)
         transform.setTranslate(mouse.x - offset.x, mouse.y - offset.y)
+        
+        if (selectedChip != document.getElementById("graphGroup")) {
+            posFromCenter.x -= (mouse.x - offset.x)
+            posFromCenter.y -= (mouse.y - offset.y)
+            changeBG(posFromCenter.x, posFromCenter.y)
+        }
     }
 }
 
@@ -92,8 +164,11 @@ function dragEnd(evt) {
 
 function makeDraggable(evt) {
     svg = evt.target
+    SVGGroup = document.getElementById("graphGroup")
+
     svg.addEventListener("mousedown", dragStart)
     svg.addEventListener("mousemove", dragMove)
     svg.addEventListener("mouseup", dragEnd)
     svg.addEventListener("mouseleave", dragEnd)
+    svg.addEventListener("wheel", scrolling)
 }
